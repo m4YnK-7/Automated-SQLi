0af18d98091e4925e3e2196b7a65513298297a92
Mayank Singh <mayanksingh4370@gmail.com>
2025-11-09 13:39:11 +0530

    Fix DVWA

---
+++ dvwa/config/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+$_DVWA = array();
+$_DVWA['db_server']   = 'mysql';
+$_DVWA['db_database'] = 'dvwa';
+$_DVWA['db_user']     = 'dvwa';
+$_DVWA['db_password'] = 'dvwa';
+$_DVWA['db_port']     = '3306';
+$_DVWA['db_type']     = 'mysql';
+$_DVWA['recaptcha_public_key'] = '';
+$_DVWA['recaptcha_private_key'] = '';
+$_DVWA['default_security_level'] = 'low';
+?>
